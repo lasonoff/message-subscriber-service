9c26f98e2cdb3a33aeec56423f9ce145391981ff
lasonoff <lason-vrn@yandex.ru>
2023-03-17 18:08:09 +0300

    Add clear test

---
+++ src/test/java/ru/yauroff/messagesubscriber/service/impl/AgentLoaderServiceImplTest.java
@@ -0,0 +1,12 @@
+package ru.yauroff.messagesubscriber.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgentLoaderServiceImplTest {
+
+    @Test
+    void loadAll() {
+    }
+}
