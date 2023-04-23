8fab2095aee82027153fa6085a1e67cd8861e878
lasonoff <lason-vrn@yandex.ru>
2023-04-23 20:53:15 +0300

    add flux and reactive webclient

--- src/main/java/ru/yauroff/messagesubscriber/service/impl/AgentLoaderServiceImpl.java
+++ src/main/java/ru/yauroff/messagesubscriber/service/impl/AgentLoaderServiceImpl.java
@@ -1,7 +1,5 @@
 package ru.yauroff.messagesubscriber.service.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -10,18 +8,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
+import org.springframework.web.reactive.function.client.WebClient;
 import ru.yauroff.messagesubscriber.dto.AgentDTO;
 import ru.yauroff.messagesubscriber.repository.AgentRepository;
 import ru.yauroff.messagesubscriber.service.AgentLoaderService;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @Service
 @Data
@@ -40,36 +32,15 @@ public class AgentLoaderServiceImpl implements AgentLoaderService {
     @Override
     public void loadAll() throws IOException {
         log.info("Agent load url: {}", loadUrl);
-        URL url = new URL(loadUrl);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setConnectTimeout(5000);
-        con.setReadTimeout(5000);
+        WebClient webClient = WebClient.create();
 
-        int status = con.getResponseCode();
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
-        con.disconnect();
-        List<AgentDTO> agents = null;
-        try {
-            agents = objectMapper.readValue(content.toString(), new TypeReference<List<AgentDTO>>() {
-            });
-        } catch (JsonProcessingException e) {
-            log.error(e.getMessage());
-        }
-        log.info(agents.toString());
-        Flux.fromIterable(agents)
-            .map(agentDto -> agentDto.toAgent())
-            .flatMap(agent -> agentRepository.save(agent))
-            .subscribe(value -> log.info("Add agent {}", value),
-                    error -> log.error("Error {}", error.getMessage()));
+        webClient.get()
+                 .uri(loadUrl)
+                 .retrieve()
+                 .bodyToFlux(AgentDTO.class)
+                 .map(agentDto -> agentDto.toAgent())
+                 .flatMap(agent -> agentRepository.save(agent))
+                 .subscribe(value -> log.info("Add agent {}", value),
+                         error -> log.error("Error {}", error.getMessage()));
     }
 }
